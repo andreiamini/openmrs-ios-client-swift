04e7f64522111aaa8ab759ae2e5a9afceeadfdfa
lizaamini <liza.amini@gmail.com>
2014-12-01 19:11:32 +0000

    Initial Commit

---
+++ openmrs-ios-client-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  openmrs-ios-client-swift
+//
+//  Created by Liza Amini on 01/12/2014.
+//  Copyright (c) 2014 openmrs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
